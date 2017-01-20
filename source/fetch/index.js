39c78029e303912b7398c0a0ae7d3814a14a3974
Ellis Clayton <ellis@ellisclayton.info>
2017-01-20 14:43:47 +1000

    Allow fetching by document ref

--- source/fetch/index.js
+++ source/fetch/index.js
@@ -4,13 +4,14 @@ export default ({
   repository,
   type,
   single,
-  order = []
+  order = [],
+  token
 }) => {
   const orderings = order.map(({ field, asc }) => `my.${type}.${field}${asc ? ' desc' : ''}`)
 
   return Prismic.api(`https://${repository}.cdn.prismic.io/api`).then((Api) => (
     Api.form('everything')
-      .ref(Api.master())
+      .ref(token || Api.master())
       .query([
         Prismic.Predicates.at('document.type', type)
       ])
