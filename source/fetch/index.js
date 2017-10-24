8b8a394fefa666111b5a5c05fdf7640666251f96
Ellis Clayton <ellis@ellisclayton.info>
2017-10-24 11:21:14 +1000

    Update fetcher to V2 API

--- source/fetch/index.js
+++ source/fetch/index.js
@@ -1,25 +1,27 @@
-import Prismic from 'prismic.io'
+import Prismic, { Predicates } from 'prismic-javascript'
 
-export const fetchDocuments = ({
-  repository,
-  type,
-  fields = [],
-  orderings = [],
-  token
-}) => (
-  Prismic.api(`https://${repository}.cdn.prismic.io/api`).then((Api) => (
-    Api.form('everything')
-      .ref(token || Api.master())
-      .query([
-        Prismic.Predicates.at('document.type', type),
-        ...fields.map(({ field, value }) => Prismic.Predicates.at(`my.${type}.${field}`, value))
-      ])
-      .orderings(orderings)
-      .submit()
-  ))
-  .then((response) => response.results)
+const buildPredicates = (type, predicates = []) => (
+  type ? [Predicates.at('document.type', type), ...predicates] : predicates
 )
 
-export const fetchDocument = (params) => {
-  return fetchDocuments(params).then((results) => results[0])
-}
+export const fetchDocuments = ({
+  options = {}, // https://prismic.io/docs/javascript/query-the-api/query-options-reference
+  predicates, // https://prismic.io/docs/javascript/query-the-api/query-predicates-reference
+  repository,
+  token,
+  type
+}) => (
+  Prismic.getApi(`https://${repository}.cdn.prismic.io/api/v2`)
+  .then((api) => (
+    api.everything()
+    .ref(token || api.master())
+    .query(buildPredicates(type, predicates), {})
+    .submit()
+  )).then((response) => (
+    response.results
+  ))
+)
+
+export const fetchDocument = (params) => (
+  fetchDocuments(params).then((results) => results[0])
+)
