48e051a8d1649d86601c8acd81d93c6c2cb67aae
Ellis Clayton <ellis@ellisclayton.info>
2017-10-24 14:48:27 +1000

    Add V2 document deserialization

--- source/deserialize/index.js
+++ source/deserialize/index.js
@@ -1,138 +1,35 @@
+import isArray from 'lodash/isArray'
 import set from 'lodash/set'
-import reduce from 'lodash/reduce'
 import sortBy from 'lodash/sortBy'
-import striptags from 'striptags'
+import toPairs from 'lodash/toPairs'
+import { RichText } from 'prismic-reactjs'
 
-const deserializeDocument = (prismicDoc, options) => {
-  const fields = mapFieldTypesToArray(prismicDoc)
+const deserializeDocument = ({ data, ...metadata }) => {
+  const deserializedFields = deserializeFields(data)
 
-  const deserializedFields = fields.map((field) => ({
-    key: field.key,
-    value: deserializeField(prismicDoc, field, options)
-  }))
-
-  return mapFieldsToObject(deserializedFields)
-}
-
-const deserializeField = (prismicDoc, { key, type }, options = {}) => {
-  switch (type) {
-    case 'id':
-      return prismicDoc.id
-
-    case 'uid':
-      return prismicDoc.uid
-
-    case 'tags':
-      return prismicDoc.tags
-
-    case 'Text':
-    case 'Select':
-      return prismicDoc.getText(key)
-
-    case 'Number':
-      return prismicDoc.getNumber(key)
-
-    case 'StructuredText':
-      const structuredText = prismicDoc.getStructuredText(key)
-      if (key === 'title') {
-        return structuredText && structuredText.asHtml({}, (element, content) => striptags(content))
-      } else {
-        const htmlSerializer = getSerializer(key, options.htmlSerializers)
-        return structuredText && structuredText.asHtml({}, htmlSerializer)
-      }
-
-    case 'Link.web':
-      const link = prismicDoc.getLink(key)
-      return link && link.value.url
-
-    case 'Link.document':
-      return prismicDoc.getLink(key)
-
-    case 'Link.image':
-      const imageLink = prismicDoc.getLink(key)
-      return imageLink && {
-        url: imageLink.value.image.url,
-        alt: imageLink.value.image.alt
-      }
-
-    case 'Link.file':
-      const fileLink = prismicDoc.getLink(key)
-      return fileLink && fileLink.value.file.url
-
-    case 'Group':
-      const groups = prismicDoc.getGroup(key)
-      return groups.value.map((group) => deserializeDocument(group))
-
-    case 'SliceZone':
-      const { slices = [] } = prismicDoc.getSliceZone(key) || {}
-      return slices.map((slice) => {
-        // get the nested group values, or just set the slice.value if not a group
-        const {
-          value = slice.value
-        } = slice.value
-
-        const deserializedValue = Array.isArray(value) ? (
-          value.map((doc) => deserializeDocument(doc))
-        ) : value
-
-        return {
-          type: slice.sliceType,
-          value: deserializedValue
-        }
-      })
-
-    case 'Image':
-      const image = prismicDoc.getImage(key)
-      const { views = {} } = image
-
-      return image && {
-        main: image.main,
-        ...views
-      }
-
-    case 'Color':
-      return prismicDoc.getColor(key)
-
-    case 'Embed':
-      const video = prismicDoc.get(key)
-      return video && video.asHtml()
-
-    case 'Date':
-      const date = prismicDoc.get(key)
-      return date && date.value && date.value.toISOString()
-
-    case 'Timestamp':
-      const timestamp = prismicDoc.get(key)
-      return timestamp && timestamp.value && timestamp.value.toISOString()
-
-    default:
-      return type
+  return {
+    ...metadata,
+    data: mapFieldsToObject(deserializedFields)
   }
 }
 
-const mapFieldTypesToArray = ({
-  data,
-  ...props
-}) => {
-  // add document level fields
-  const documentFields = reduce(['id', 'uid', 'tags'], (fields, field) => (
-    props[field] ? (
-      fields.concat({
-        key: field,
-        type: field
-      })
-    ) : fields
-  ), [])
+const deserializeFields = (fields) => (
+  toPairs(fields).map(([ key, value ]) => ({
+    key,
+    value: deserializeField(value)
+  }))
+)
 
-  // add normal fields
-  const fields = reduce(data, (fields, value, key) => (
-    fields.concat({
-      key: key,
-      type: value.type
-    })
-  ), documentFields)
-
-  return fields
+const deserializeField = (value, options = {}) => {
+  if (isArray(value) && value.length > 0 && value[0].type) {
+    // Rich Text
+    return RichText.render(value)
+  } else if (isArray(value) && value.length > 0 && !value[0].type) {
+    // Groups
+    return value.map((group) => deserializeFields(group))
+  } else {
+    return value
+  }
 }
 
 const mapFieldsToObject = (fields) => (
@@ -143,10 +40,6 @@ const mapFieldsToObject = (fields) => (
     const nestedStructure = keyName.split('-').join('.')
     return set(data, nestedStructure, value)
   }, {})
-)
-
-const getSerializer = (key, serializers = {}) => (
-  serializers[key]
 )
 
 export default deserializeDocument
