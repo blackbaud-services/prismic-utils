6a7662d0f7541ccbba788a3a12f036d921d3c7e5
Ellis Clayton <ellis@ellisclayton.info>
2017-10-25 11:35:47 +1000

    Allow deserialization without React

--- source/deserialize/index.js
+++ source/deserialize/index.js
@@ -3,11 +3,12 @@ import set from 'lodash/set'
 import sortBy from 'lodash/sortBy'
 import striptags from 'striptags'
 import toPairs from 'lodash/toPairs'
-import { RichText } from 'prismic-reactjs'
+import { RichText as ReactText } from 'prismic-reactjs'
+import { RichText } from 'prismic-dom'
 import { renderToStaticMarkup } from 'react-dom/server'
 
-const deserializeDocument = ({ data, ...metadata }) => {
-  const deserializedFields = deserializeFields(data)
+const deserializeDocument = ({ data, ...metadata }, options = { react: true }) => {
+  const deserializedFields = deserializeFields(data, options)
 
   return {
     ...metadata,
@@ -15,32 +16,34 @@ const deserializeDocument = ({ data, ...metadata }) => {
   }
 }
 
-const deserializeFields = (fields) => (
+const deserializeFields = (fields, options) => (
   toPairs(fields).map(([ key, value ]) => ({
     key,
-    value: deserializeField(value)
+    value: deserializeField(value, options)
   }))
 )
 
-const deserializeField = (value, options = {}) => {
+const deserializeField = (value, options) => {
   if (isArray(value) && value.length > 0 && value[0].type) {
     // Rich Text
-    return RichText.render(value)
+    return options.react
+      ? ReactText.render(value)
+      : RichText.asHtml(value)
   } else if (isArray(value) && value.length > 0 && value[0].slice_type) {
     // Slice
     return value.map((slice) => ({
       type: slice.slice_type,
       label: slice.slice_label,
       primary: mapFieldsToObject(
-        deserializeFields(slice.primary)
+        deserializeFields(slice.primary, options)
       ),
       items: slice.items.map((item) => (
-        mapFieldsToObject(deserializeFields(item))
+        mapFieldsToObject(deserializeFields(item, options))
       ))
     }))
   } else if (isArray(value) && value.length > 0 && !value[0].type) {
     // Groups
-    return value.map((group) => deserializeFields(group))
+    return value.map((group) => deserializeFields(group, options))
   } else {
     return value
   }
