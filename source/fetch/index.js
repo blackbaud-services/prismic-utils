9a233401396d9e25fa836b18374168b1871db514
danhenderson <danhenderson.web@gmail.com>
2020-06-12 07:31:36 +1000

    Recursively fetch based on limit

--- source/fetch/index.js
+++ source/fetch/index.js
@@ -5,22 +5,40 @@ const buildPredicates = (type, predicates = []) => (
 )
 
 export const fetchDocuments = ({
+  documents = [],
+  limit = 20,
   pageSize = 20,
+  page = 1,
   predicates, // https://prismic.io/docs/javascript/query-the-api/query-predicates-reference
   repository,
   token,
   type
 }) => (
   Prismic.getApi(`https://${repository}.cdn.prismic.io/api/v2`)
-  .then((api) => (
-    api.everything()
-    .ref(token || api.master())
-    .query(buildPredicates(type, predicates), {})
-    .pageSize(pageSize)
-    .submit()
-  )).then((response) => (
-    response.results
-  ))
+    .then((api) => (
+      api.everything()
+      .ref(token || api.master())
+      .query(buildPredicates(type, predicates), {})
+      .pageSize(pageSize)
+      .page(page)
+      .submit()
+    )).then((response) => {
+      const allDocs = [...documents, ...response.results]
+
+      if (allDocs.length < limit && response.next_page) {
+        return fetchDocuments({
+          documents: allDocs,
+          page: page + 1,
+          pageSize,
+          predicates,
+          repository,
+          token,
+          type
+        })
+      } else {
+        return allDocs
+      }
+    })
 )
 
 export const fetchDocument = (params) => (
